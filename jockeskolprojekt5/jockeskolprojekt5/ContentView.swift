4f913d64689f03dd8e3e7699138db3bbd01dfec7
Joakim Sjöstedt <joakim.sjostedt@softhouse.se>
2023-11-02 23:59:53 +0100

    kolla nuuuu

---
+++ jockeskolprojekt5/jockeskolprojekt5/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  jockeskolprojekt5
+//
+//  Created by Joakim Sjöstedt on 2023-11-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ jockeskolprojekt5/jockeskolprojekt5/jockeskolprojekt5App.swift
@@ -0,0 +1,17 @@
+//
+//  jockeskolprojekt5App.swift
+//  jockeskolprojekt5
+//
+//  Created by Joakim Sjöstedt on 2023-11-02.
+//
+
+import SwiftUI
+
+@main
+struct jockeskolprojekt5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
